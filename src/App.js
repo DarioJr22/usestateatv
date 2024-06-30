3e6f04783a35abb7c80d99b5b71b57e139e20b6f
dario.junior <dariojr1233@gmail.com>
2024-06-29 21:13:36 -0300

    React hooks training

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
 
 function App() {
+
+  const [tasks, useTasks] = useState([])
+
+
+  const [input, setInput] = useState('')
+
+  function AdicionaItem(){
+      useTasks([...tasks,input])
+      setInput('')
+  }
+
+
+  function preencheCampo(e){
+    setInput(e.target.value)
+  }
+
+  function RecuperItens(){
+    console.log([...JSON.parse(window.localStorage.getItem('userTasks'))]);
+    useTasks([...JSON.parse(window.localStorage.getItem('userTasks'))])
+  }
+  
+//É executado no momento de montagem do componente
+  useEffect(()=>{
+    RecuperItens()
+  },
+  []
+)
+
+  //Nesse caso é executado sempre que esse elemento for atualizado
+  useEffect(()=>{
+    window.localStorage.setItem('userTasks',JSON.stringify(tasks))
+
+  },[tasks])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>
+    <h1>
+      Olá, vamos falar sobre hooks !
+    </h1>
+
+    <ul>
+    {
+      tasks.map(item => 
+        <li key={item}>{item}</li>
+      )
+    }
+    <div>
+      <input onChange={preencheCampo} type='text' />
+      <button onClick={AdicionaItem}>oche</button>
     </div>
+    </ul>
+   
+   </div>
   );
 }
 
